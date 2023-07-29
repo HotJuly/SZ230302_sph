972986f326ab6564c5b2862110ae182dd6da207c
HotJuly <949588380@qq.com>
2023-07-29 11:08:32 +0800

    68.放大镜插件使用

--- 2.代码/sph/src/views/Detail/index.vue
+++ 2.代码/sph/src/views/Detail/index.vue
@@ -5,56 +5,34 @@
     <section class="con">
       <!-- 导航路径区域 -->
       <div class="conPoin">
-        <span>手机、数码、通讯</span>
-        <span>手机</span>
-        <span>Apple苹果</span>
-        <span>iphone 6S系类</span>
+        <span
+        v-for="num in 3"
+        :key="num"
+        >
+        {{categoryView[`category${num}Name`]}}
+        </span>
       </div>
       <!-- 主要内容区域 -->
       <div class="mainCon">
         <!-- 左侧放大镜区域 -->
         <div class="previewWrap">
           <!--放大镜效果-->
-          <Zoom />
+          <Zoom :skuDefaultImg="skuInfo.skuDefaultImg"/>
           <!-- 小图列表 -->
           <ImageList />
         </div>
         <!-- 右侧选择区域布局 -->
         <div class="InfoWrap">
           <div class="goodsDetail">
-            <h3 class="InfoName">Apple iPhone 6s（A1700）64G玫瑰金色 移动通信电信4G手机</h3>
+            <h3 class="InfoName">{{skuInfo.skuName}}</h3>
             <p class="news">推荐选择下方[移动优惠购],手机套餐齐搞定,不用换号,每月还有花费返</p>
             <div class="priceArea">
               <div class="priceArea1">
                 <div class="title">价&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;格</div>
                 <div class="price">
                   <i>¥</i>
-                  <em>5299</em>
-                  <span>降价通知</span>
+                  <em>{{skuInfo.price}}</em>
                 </div>
-                <div class="remark">
-                  <i>累计评价</i>
-                  <em>65545</em>
-                </div>
-              </div>
-              <div class="priceArea2">
-                <div class="title">
-                  <i>促&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;销</i>
-                </div>
-                <div class="fixWidth">
-                  <i class="red-bg">加价购</i>
-                  <em class="t-gray">满999.00另加20.00元，或满1999.00另加30.00元，或满2999.00另加40.00元，即可在购物车换购热销商品</em>
-                </div>
-              </div>
-            </div>
-            <div class="support">
-              <div class="supportArea">
-                <div class="title">支&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;持</div>
-                <div class="fixWidth">以旧换新，闲置手机回收 4G套餐超值抢 礼品购</div>
-              </div>
-              <div class="supportArea">
-                <div class="title">配 送 至</div>
-                <div class="fixWidth">广东省 深圳市 宝安区</div>
               </div>
             </div>
           </div>
@@ -62,29 +40,16 @@
           <div class="choose">
             <div class="chooseArea">
               <div class="choosed"></div>
-              <dl>
-                <dt class="title">选择颜色</dt>
-                <dd changepirce="0" class="active">金色</dd>
-                <dd changepirce="40">银色</dd>
-                <dd changepirce="90">黑色</dd>
-              </dl>
-              <dl>
-                <dt class="title">内存容量</dt>
-                <dd changepirce="0" class="active">16G</dd>
-                <dd changepirce="300">64G</dd>
-                <dd changepirce="900">128G</dd>
-                <dd changepirce="1300">256G</dd>
-              </dl>
-              <dl>
-                <dt class="title">选择版本</dt>
-                <dd changepirce="0" class="active">公开版</dd>
-                <dd changepirce="-1000">移动版</dd>
-              </dl>
-              <dl>
-                <dt class="title">购买方式</dt>
-                <dd changepirce="0" class="active">官方标配</dd>
-                <dd changepirce="-240">优惠移动版</dd>
-                <dd changepirce="-390">电信优惠版</dd>
+              <dl
+                v-for="saleAttr in spuSaleAttrList"
+                :key="saleAttr.id"
+              >
+                <dt class="title">选择{{saleAttr.saleAttrName}}</dt>
+                <dd 
+                class="active"
+                v-for="attrValue in saleAttr.spuSaleAttrValueList"
+                :key="attrValue.id"
+                >{{attrValue.saleAttrValueName}}</dd>
               </dl>
             </div>
             <div class="cartWrap">
@@ -96,246 +61,6 @@
               <div class="add">
                 <a href="javascript:">加入购物车</a>
               </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <!-- 内容详情页 -->
-    <section class="product-detail">
-      <aside class="aside">
-        <div class="tabWraped">
-          <h4 class="active">相关分类</h4>
-          <h4>推荐品牌</h4>
-        </div>
-        <div class="tabContent">
-          <div class="tab-pane active">
-            <ul class="partList">
-              <li>手机</li>
-              <li>手机壳</li>
-              <li>内存卡</li>
-              <li>Iphone配件</li>
-              <li>贴膜</li>
-              <li>手机耳机</li>
-              <li>移动电源</li>
-              <li>平板电脑</li>
-            </ul>
-            <ul class="goodsList">
-              <li>
-                <div class="list-wrap">
-                  <div class="p-img">
-                    <img src="./images/part01.png" />
-                  </div>
-                  <div class="attr">Apple苹果iPhone 6s (A1699) </div>
-                  <div class="price">
-                    <em>¥</em>
-                    <i>6088.00</i>
-                  </div>
-                  <div class="operate">
-                    <a href="javascript:void(0);">加入购物车</a>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="list-wrap">
-                  <div class="p-img">
-                    <img src="./images/part02.png" />
-                  </div>
-                  <div class="attr">
-                    <em>Apple苹果iPhone 6s (A1699)</em>
-                  </div>
-                  <div class="price">
-                    <strong>
-                      <em>¥</em>
-                      <i>6088.00</i>
-                    </strong>
-                  </div>
-                  <div class="operate">
-                    <a href="javascript:void(0);">加入购物车</a>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="list-wrap">
-                  <div class="p-img">
-                    <img src="./images/part03.png" />
-                  </div>
-                  <div class="attr">
-                    <em>Apple苹果iPhone 6s (A1699)</em>
-                  </div>
-                  <div class="price">
-                    <strong>
-                      <em>¥</em>
-                      <i>6088.00</i>
-                    </strong>
-                  </div>
-                  <div class="operate">
-                    <a href="javascript:void(0);">加入购物车</a>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="list-wrap">
-                  <div class="p-img">
-                    <img src="./images/part02.png" />
-                  </div>
-                  <div class="attr">
-                    <em>Apple苹果iPhone 6s (A1699)</em>
-                  </div>
-                  <div class="price">
-                    <strong>
-                      <em>¥</em>
-                      <i>6088.00</i>
-                    </strong>
-                  </div>
-                  <div class="operate">
-                    <a href="javascript:void(0);">加入购物车</a>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="list-wrap">
-                  <div class="p-img">
-                    <img src="./images/part03.png" />
-                  </div>
-                  <div class="attr">
-                    <em>Apple苹果iPhone 6s (A1699)</em>
-                  </div>
-                  <div class="price">
-                    <strong>
-                      <em>¥</em>
-                      <i>6088.00</i>
-                    </strong>
-                  </div>
-                  <div class="operate">
-                    <a href="javascript:void(0);">加入购物车</a>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-          <div class="tab-pane">
-            <p>推荐品牌</p>
-          </div>
-        </div>
-      </aside>
-      <div class="detail">
-        <div class="fitting">
-          <h4 class="kt">选择搭配</h4>
-          <div class="good-suits">
-            <div class="master">
-              <img src="./images/l-m01.png" />
-              <p>￥5299</p>
-              <i>+</i>
-            </div>
-            <ul class="suits">
-              <li class="suitsItem">
-                <img src="./images/dp01.png" />
-                <p>Feless费勒斯VR</p>
-                <label>
-                  <input type="checkbox" value="39">
-                  <span>39</span>
-                </label>
-              </li>
-              <li class="suitsItem">
-                <img src="./images/dp02.png" />
-                <p>Feless费勒斯VR</p>
-                <label>
-                  <input type="checkbox" value="50">
-                  <span>50</span>
-                </label>
-              </li>
-              <li class="suitsItem">
-                <img src="./images/dp03.png" />
-                <p>Feless费勒斯VR</p>
-                <label>
-                  <input type="checkbox" value="59">
-                  <span>59</span>
-                </label>
-              </li>
-              <li class="suitsItem">
-                <img src="./images/dp04.png" />
-                <p>Feless费勒斯VR</p>
-                <label>
-                  <input type="checkbox" value="99">
-                  <span>99</span>
-                </label>
-              </li>
-            </ul>
-            <div class="result">
-              <div class="num">已选购0件商品</div>
-              <div class="price-tit">
-                套餐价
-              </div>
-              <div class="price">￥5299</div>
-              <button class="addshopcar">加入购物车</button>
-            </div>
-          </div>
-        </div>
-        <div class="intro">
-          <ul class="tab-wraped">
-            <li class="active">
-              <a href="###">
-                商品介绍
-              </a>
-            </li>
-            <li>
-              <a href="###">
-                规格与包装
-              </a>
-            </li>
-            <li>
-              <a href="###">
-                售后保障
-              </a>
-            </li>
-            <li>
-              <a href="###">
-                商品评价
-              </a>
-            </li>
-            <li>
-              <a href="###">
-                手机社区
-              </a>
-            </li>
-          </ul>
-          <div class="tab-content">
-            <div id="one" class="tab-pane active">
-              <ul class="goods-intro">
-                <li>分辨率：1920*1080(FHD)</li>
-                <li>后置摄像头：1200万像素</li>
-                <li>前置摄像头：500万像素</li>
-                <li>核 数：其他</li>
-                <li>频 率：以官网信息为准</li>
-                <li>品牌： Apple</li>
-                <li>商品名称：APPLEiPhone 6s Plus</li>
-                <li>商品编号：1861098</li>
-                <li>商品毛重：0.51kg</li>
-                <li>商品产地：中国大陆</li>
-                <li>热点：指纹识别，Apple Pay，金属机身，拍照神器</li>
-                <li>系统：苹果（IOS）</li>
-                <li>像素：1000-1600万</li>
-                <li>机身内存：64GB</li>
-              </ul>
-              <div class="intro-detail">
-                <img src="./images/intro01.png" />
-                <img src="./images/intro02.png" />
-                <img src="./images/intro03.png" />
-              </div>
-            </div>
-            <div id="two" class="tab-pane">
-              <p>规格与包装</p>
-            </div>
-            <div id="three" class="tab-pane">
-              <p>售后保障</p>
-            </div>
-            <div id="four" class="tab-pane">
-              <p>商品评价</p>
-            </div>
-            <div id="five" class="tab-pane">
-              <p>手机社区</p>
             </div>
           </div>
         </div>
@@ -385,8 +110,23 @@
               如果在created阻塞10秒,后续页面的挂载也会慢10秒
           
     */
-    created(){
-      this.$API.detail.reqDetailInfo(this.$route.params.id);
+    data(){
+      return{
+        // 用于存储当前商品的三级分类信息
+        categoryView:{},
+        // 用于存储当前商品的相关信息
+        skuInfo:{},
+        // 用于存储当前商品可选的销售属性,通过不同的销售属性组合可以得到另外一个兄弟商品
+        spuSaleAttrList:[]
+      }
+    },
+    async created(){
+      const {categoryView:categoryView,skuInfo,spuSaleAttrList} = await this.$API.detail.reqDetailInfo(this.$route.params.id);
+      // console.log(result)
+      this.categoryView = categoryView;
+      this.skuInfo = skuInfo;
+      this.spuSaleAttrList = spuSaleAttrList;
+      // console.log(this.categoryView)
     },
     mounted(){
     },
